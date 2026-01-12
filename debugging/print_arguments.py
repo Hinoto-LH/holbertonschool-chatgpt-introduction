c76494f7a2128c94bed8a1dab06e9fd47964e117
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-12 12:04:08 +0100

    fix file

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -3,4 +3,5 @@ import sys
 
 # On commence à l'index 1 jusqu'à la fin
 for arg in sys.argv[1:]:
-    print(f"Argument reçu : {arg}")
+    print(f"{arg}") # Une f-string permet d’insérer directement des variables ou 
+                    # des expressions dans une chaîne de caractères, de manière simple et lisible.
